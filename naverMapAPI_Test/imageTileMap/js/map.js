092e086ca2516b4828fa1fb67822cc7a9a5def94
LeeSangYeop <infightersy@naver.com>
2018-11-21 18:12:22 +0900

    2018-11-21 18:12 이미지 타일 지도 유형 추가..

---
+++ naverMapAPI_Test/imageTileMap/js/map.js
@@ -0,0 +1,30 @@
+function initMap() { // 지도 생성
+  map = new naver.maps.Map('map', {
+    center: new naver.maps.LatLng(37.3595704, 127.10539),
+    zoom: 10,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+        style: naver.maps.MapTypeControlStyle.DROPDOWN
+    }
+  });
+  console.log("지도 생성 성공");
+
+  var openStreetMapType  = new naver.maps.ImageMapType({
+    name: 'OSM',
+    minZoom: 0,
+    maxZoom: 19,
+    tileSize: new naver.maps.Size(256, 256),
+    projection: naver.maps.EPSG3857,
+    repeatX: true,
+    tileSet: "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    provider: [{
+        title: " /OpenStreetMap",
+        link: "http://www.openstreetmap.org/copyright"      
+    }]
+  });
+  // 맵타입 설정
+  map.mapTypes.set('osm', openStreetMapType);
+  map.setMapTypeId('osm');
+ }
+
+ 
